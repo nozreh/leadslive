8d5c18012befddb30a051aae90fd73ee883fa2f2
unknown <hgarlan@NIDIGIHGARLAN81.users.sphinet.com.sg>
2015-04-24 01:46:59 +0800

    fixed the relative path of default laravel public

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 |
 */
 
-require __DIR__.'/bootstrap/autoload.php';
+require __DIR__.'/../bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -26,7 +26,7 @@ require __DIR__.'/bootstrap/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
